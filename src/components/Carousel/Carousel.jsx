bc28c00f8d8f764543e3385b0e6beb1129192f4b
Auréliane <aureliane.gailliegue@gmail.com>
2022-05-10 18:18:10 +0200

    Remove carousel navigation if single picture

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -18,31 +18,49 @@ function Carousel(props) {
 
    return (
       <div className="carousel_container">
-         <div className="carousel_previous" onClick={toggleClickPrevious}></div>
-         {props.pictures.map((picture, index) => {
-            return (
+         {length === 1 ? (
+            <div className="carousel_img_container">
+               <img
+                  className="carousel_img"
+                  src={props.pictures[0]}
+                  alt={'Logement'}
+               />
+            </div>
+         ) : (
+            <div>
                <div
-                  className="carousel_img_container"
-                  key={index}
-                  style={{
-                     transform:
-                        index === current
-                           ? 'translateX(0)'
-                           : 'translateX(' + translate + ')',
-                  }}
-               >
-                  {index === current && (
-                     <img
-                        className="carousel_img"
-                        src={picture}
-                        alt={'Image n°' + (current + 1) + ' du logement'}
-                     />
-                  )}
-               </div>
-            )
-         })}
-         <p className="carousel_count">{current + 1 + '/' + length}</p>
-         <div className="carousel_next" onClick={toggleClickNext}></div>
+                  className="carousel_previous"
+                  onClick={toggleClickPrevious}
+               ></div>
+               {props.pictures.map((picture, index) => {
+                  return (
+                     <div
+                        className="carousel_img_container"
+                        key={index}
+                        style={{
+                           transform:
+                              index === current
+                                 ? 'translateX(0)'
+                                 : 'translateX(' + translate + ')',
+                        }}
+                     >
+                        {index === current && (
+                           <img
+                              className="carousel_img"
+                              src={picture}
+                              alt={'Image n°' + (current + 1) + ' du logement'}
+                           />
+                        )}
+                     </div>
+                  )
+               })}
+               <p className="carousel_count">{current + 1 + '/' + length}</p>
+               <div
+                  className="carousel_next"
+                  onClick={toggleClickNext}
+               ></div>{' '}
+            </div>
+         )}
       </div>
    )
 }
